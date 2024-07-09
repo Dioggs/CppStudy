30688fff72863febcc9d8736584ee6aee6bcf05d
Dioggs <diogo.araldo@taking.com.br>
2024-07-09 19:09:44 -0300

    chore: initial commit

---
+++ header_files/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "heyyy" << std::endl;
+
+  // Figure out how to set a new path for the creation of the binary
+
+  // Figure out how to import things from other files
+
+  // Figure out with header files exist
+
+  // Figure out how to compile my project more easily
+
+}
